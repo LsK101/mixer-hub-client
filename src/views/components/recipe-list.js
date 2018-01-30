4e5346cf15d181aa418e6c5431399aa3df8b7399
Patrick Panahon <patrick.panahon@gmail.com>
2018-01-30 16:21:37 -0500

    rewrote search function to make it case-insensitive and account for words inputted out of order

--- src/views/components/recipe-list.js
+++ src/views/components/recipe-list.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+function checkStringEquality(searchQuery, recipeName) {
+	const queryStrings = searchQuery.toLowerCase().split(" ").filter(Boolean);
+	const recipeNameLowerCase = recipeName.toLowerCase();
+	for (let i = 0; i < queryStrings.length; i++) {
+		if (!recipeNameLowerCase.includes(queryStrings[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
 function RecipeList(props) {
 	const recipes = props.recipes
-	.filter(recipe => recipe.name.toLowerCase().includes(props.query))
+	.filter(recipe => checkStringEquality(props.query,recipe.name))
 	.map((recipe,index) =>
 		<div className="recipe-result col-6">
 			<strong>{recipe.name}</strong>: {recipe.ABV}%ABV
