f08a2787ca65d5f17efa482eef59c82e745cd03c
Patrick Panahon <patrick.panahon@gmail.com>
2018-02-06 12:26:25 -0500

    added error catch to api request

--- src/views/browse-recipes.js
+++ src/views/browse-recipes.js
@@ -27,7 +27,8 @@ class BrowseRecipes extends Component {
               return this.setState({
                 "recipeData": recipeData
               });
-            });
+            })
+            .catch(err => alert(err));
   }
 
   render() {
