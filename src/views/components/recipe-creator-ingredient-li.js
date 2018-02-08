694e5884f5db48d5436f5e875304e05c546cff46
Patrick Panahon <patrick.panahon@gmail.com>
2018-02-08 15:39:38 -0500

    slightly modified ingredient list element layout in recipe creator for mobile display bug

--- src/views/components/recipe-creator-ingredient-li.js
+++ src/views/components/recipe-creator-ingredient-li.js
@@ -6,14 +6,20 @@ import NumberInput from './number-input';
 function IngredientListElement(props) {
 	return (
 		<li className={props.className}>
-			Ingredient:
-			<TextInput value={props.valueIngredient} 
+			<span>Ingredient:</span><br/>
+			<TextInput divClassName="ingredient-input-container"
+				value={props.valueIngredient} 
 				onChange={value => props.onChangeIngredient(value)} />
+
+			<br/>
+
 			<NumberInput divClassName="ABV-input-container" className="ABV-input"
 				min={0} max={100} value={props.valueABV}
 				onChange={value => props.onChangeABV(value)} />
 			<span>% ABV</span>
+
 			<br/>
+
 			<NumberInput divClassName="parts-input-container" className="parts-input"
 				min={0} max={99999} value={props.valueParts}
 				onChange={value => props.onChangeParts(value)} />
