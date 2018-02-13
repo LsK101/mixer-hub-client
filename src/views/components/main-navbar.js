fa988e446badff8fbe80360ba2c179b45fb42c5a
Patrick Panahon <patrick.panahon@gmail.com>
2018-02-13 14:43:56 -0500

    minor styling changes

--- src/views/components/main-navbar.js
+++ src/views/components/main-navbar.js
@@ -8,15 +8,15 @@ class MainNavBar extends Component {
       	<div className="main-navbar-container">
       		<div className="nav-links-container col-12">
       			<ul className="nav-links">
-              <li><Link to="/main">Dashboard</Link></li>
-              <li><Link to="/create">Create Recipe</Link></li>
-              <li><Link to="/browse">Browse Recipes</Link></li>
+              <li><Link to="/main" className="nav-link">Dashboard</Link></li>
+              <li><Link to="/create" className="nav-link">Create Recipe</Link></li>
+              <li><Link to="/browse" className="nav-link">Browse Recipes</Link></li>
               {this.props.currentUser === 'demo' ?
               null :
-              <li><Link to="/manage">Manage Recipes</Link></li>}
+              <li><Link to="/manage" className="nav-link">Manage Recipes</Link></li>}
               {this.props.currentUser === 'demo' ?
-              <li><Link to="/" onClick={this.props.logout}>Back To Home</Link></li> :
-              <li><Link to="/" onClick={this.props.logout}>Log Out</Link></li>}
+              <li><Link to="/" className="nav-link" onClick={this.props.logout}>Back To Home</Link></li> :
+              <li><Link to="/" className="nav-link" onClick={this.props.logout}>Log Out</Link></li>}
             </ul>
       		</div>
       	</div>
